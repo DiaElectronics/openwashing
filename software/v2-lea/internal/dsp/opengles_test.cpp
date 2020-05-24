78adec3d9b269156f8e72b4822e3badb350c06e0
Roman Fedyashov <roman.fedyashov@gmail.com>
2020-05-25 00:11:25 +0700

    test faked up

--- software/v2-lea/internal/dsp/opengles_test.cpp
+++ software/v2-lea/internal/dsp/opengles_test.cpp
@@ -37,29 +37,35 @@ int main(int argc, const char **argv)
                 // Use our shader
                 glUseProgram(programID);
 
+                for (int i=0; i<4;i++) {
+                        for (int j=0; j<2;j++) {
+
+                        
                 // Bind our texture in Texture Unit 0
-                glActiveTexture(GL_TEXTURE0);
-                glBindTexture(GL_TEXTURE_2D, Texture);
-                // Set our "myTextureSampler" sampler to user Texture Unit 0
-                glUniform1i(TextureID, 0);
+                        glActiveTexture(GL_TEXTURE0);
+                        glBindTexture(GL_TEXTURE_2D, Texture);
+                        // Set our "myTextureSampler" sampler to user Texture Unit 0
+                        glUniform1i(TextureID, 0);
 
-                // 1rst attribute buffer : vertices
-//                glEnableVertexAttribArray(vertexPosition_modelspaceID);
-//                glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-                glVertexAttribPointer(
-                        0, //vertexPosition_modelspaceID, // The attribute we want to configure
-                        3,                  // size
-                        GL_FLOAT,           // type
-                        GL_FALSE,           // normalized?
-                        0,                  // stride
-                        g_vertex_buffer_data // (void*)0            // array buffer offset
-                );
+                        // 1rst attribute buffer : vertices
+        //                glEnableVertexAttribArray(vertexPosition_modelspaceID);
+        //                glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+                        glVertexAttribPointer(
+                                0, //vertexPosition_modelspaceID, // The attribute we want to configure
+                                3,                  // size
+                                GL_FLOAT,           // type
+                                GL_FALSE,           // normalized?
+                                0,                  // stride
+                                g_vertex_buffer_data // (void*)0            // array buffer offset
+                        );
 
-// see above glEnableVertexAttribArray(vertexPosition_modelspaceID);
-	   glEnableVertexAttribArray ( 0 );
+        // see above glEnableVertexAttribArray(vertexPosition_modelspaceID);
+                glEnableVertexAttribArray ( 0 );
 
-                // Draw the triangle !
-                glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+                        // Draw the triangle !
+                        glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+                }
+                }
         	updateScreen();
         }
 
