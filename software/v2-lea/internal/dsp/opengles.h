d505bf75c609bfe2961a66a8bc785ecc686650bd
Roman Fedyashov <roman.fedyashov@gmail.com>
2020-05-25 01:37:24 +0700

    small fix

--- software/v2-lea/internal/dsp/opengles.h
+++ software/v2-lea/internal/dsp/opengles.h
@@ -135,9 +135,9 @@ namespace DiaDSP {
     int DisplayImage(DiaApp::Image *img, DiaApp::FloatPair offset, DiaApp::FloatPair size) {
         return -1;
     }
-    static int _displayImage(void *obj, DiaApp::FloatPair offset, DiaApp::FloatPair size) {
+    static int _displayImage(void *obj, DiaApp::Image *img, DiaApp::FloatPair offset, DiaApp::FloatPair size) {
       OpenGLESRenderer *r = (OpenGLESRenderer *) obj;
-      return r->DisplayImage(offset, size);
+      return r->DisplayImage(img, offset, size);
     }
 
     void SwapFrame() {
